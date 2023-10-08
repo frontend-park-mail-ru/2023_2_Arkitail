6cad0dad374debe020bbea30b5612c89ee67e758
@gnatenko99 <tanya-g99@ya.ru>
2023-10-08 19:37:06 +0300

    Сделана валидация входа

--- source/src/js/login-form.js
+++ source/src/js/login-form.js
@@ -1,7 +1,10 @@
+const DATA_ERROR = "Неверный логин или пароль";
+const LOGIN_SERVER_ERROR = "Server error";
+
 class LoginForm {
-    constructor(parent) {
-        this.parent = parent
-        this.template = Handlebars.compile(`
+  constructor(parent) {
+    this.parent = parent;
+    this.template = Handlebars.compile(`
         <figure class="logo">
             <img src="/static/img/logo.svg" alt="GoTo" />
             <figcaption>
@@ -30,8 +33,73 @@ class LoginForm {
                 <span class="goto-signup-link">Зарегестрироваться</span>
             </p>
         </div>
-        `)
+    `);
 
-        this.parent.innerHTML = this.template()
+    this.parent.innerHTML = this.template();
+
+    this.validationMsg = this.parent.querySelector("[validation-msg]");
+    this.inputs = this.parent.querySelector(".goto-form form").elements;
+
+    this.arrInputs = [this.inputs["login"], this.inputs["password"]];
+
+    this.parent
+      .querySelector(".goto-form form")
+      .addEventListener("submit", (event) => {
+        event.preventDefault();
+        this.login();
+      });
+  }
+
+  login() {
+    if (context.activePage != "login") {
+      console.error("You are not on login page");
+      return null;
     }
+
+    if (context.authenticated.pending) {
+      console.error("[login()] Authentication request already pending");
+      return null;
+    }
+
+    const method = "POST";
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    const body = JSON.stringify({
+      login: this.inputs["login"].value,
+      password: this.inputs["password"].value,
+    });
+
+    context.authenticated.pending = true;
+
+    fetch(API_V1_URL + "login", {
+      method: method,
+      headers: headers,
+      body: body,
+    }).then((response) => {
+      if (response.status == 200) {
+        this.arrInputs.forEach((input) => {
+          input.value = "";
+          input.style["border-width"] = null;
+          input.style["border-color"] = null;
+        });
+        this.validationMsg.innerText = "";
+        context.authenticated.status = true;
+        console.log("Login succeed");
+      } else if (response.status == 401) {
+        this.validationMsg.innerText = DATA_ERROR;
+        this.arrInputs.forEach((input) => {
+          input.style["border-width"] = "2px";
+          input.style["border-color"] = "red";
+        });
+        context.authenticated.status = false;
+      } else {
+        this.validationMsg.innerText = LOGIN_SERVER_ERROR;
+        context.authenticated.status = false;
+        console.error("Login fatal error");
+      }
+
+      context.authenticated.pending = false;
+    });
+  }
 }
--- source/src/js/signup-form.js
+++ source/src/js/signup-form.js
@@ -10,14 +10,14 @@ const EMAIL_ERROR = "Введена некоректная почта";
 const LENGTH_PASSWORD_TEMPLATE = /^\w{4,32}$/;
 const LENGTH_PASSWORD_ERROR = "Длина пароля должна быть от 4 до 32";
 
-const REPEAT_PASSWORD_ERROR = "Пароли не совпадают"
+const REPEAT_PASSWORD_ERROR = "Пароли не совпадают";
 
-const USER_ALREADY_EXISTS_ERROR = "Пользователь уже существует"
-const SERVER_ERROR = "Server error"
+const USER_ALREADY_EXISTS_ERROR = "Пользователь уже существует";
+const SIGNUP_SERVER_ERROR = "Server error";
 
 class SignupForm {
   constructor(parent) {
-    this.parent = parent
+    this.parent = parent;
     this.template = Handlebars.compile(`
       <figure class="logo">
         <img src="/static/img/logo.svg" alt="GoTo" />
@@ -68,131 +68,129 @@ class SignupForm {
           Уже есть аккаунт? <span class="goto-login-link">Войти</span>
         </p>
       </div>
-    `)
+    `);
 
-    this.parent.innerHTML = this.template()
+    this.parent.innerHTML = this.template();
 
-    this.inputs = parent.querySelector('.goto-form form').elements;
+    this.inputs = parent.querySelector(".goto-form form").elements;
 
-    this.validationMsg = this.parent.querySelector('[validation-msg]')
+    this.validationMsg = this.parent.querySelector("[validation-msg]");
 
-    this.parent.querySelector('.goto-form form').addEventListener('submit', event => {
+    this.parent
+      .querySelector(".goto-form form")
+      .addEventListener("submit", (event) => {
+        event.preventDefault();
 
-      event.preventDefault();
+        const method = "POST";
 
-      const method = 'POST';
+        const headers = {
+          "Content-Type": "application/json",
+        };
 
-      const headers = {
-        'Content-Type': 'application/json'
-      };
+        const inputs_to_validate = [
+          {
+            target: this.inputs["repeat-password"],
+            templates: [
+              {
+                template: new RegExp("^" + this.inputs["password"].value + "$"),
+                error: REPEAT_PASSWORD_ERROR,
+              },
+            ],
+          },
+          {
+            target: this.inputs["password"],
+            templates: [
+              {
+                template: LENGTH_PASSWORD_TEMPLATE,
+                error: LENGTH_PASSWORD_ERROR,
+              },
+            ],
+          },
+          {
+            target: this.inputs["email"],
+            templates: [
+              {
+                template: EMAIL_TEMPLATE,
+                error: EMAIL_ERROR,
+              },
+            ],
+          },
+          {
+            target: this.inputs["name"],
+            templates: [
+              {
+                template: LOGIN_TEMPLATE,
+                error: LOGIN_ERROR,
+              },
+              {
+                template: LENGTH_LOGIN_TEMPLATE,
+                error: LENGTH_LOGIN_ERROR,
+              },
+            ],
+          },
+        ];
 
-      const inputs_to_validate = [
-        {
-          target: this.inputs['repeat-password'],
-          templates: [
-            {
-              template: new RegExp('^' + this.inputs['password'].value + '$'),
-              error: REPEAT_PASSWORD_ERROR,
-            },
-          ],
+        if (!this.validateInputs(inputs_to_validate)) {
+          console.error("Some invalid inputs");
+          return;
+        }
 
-        },
-        {
-          target: this.inputs['password'],
-          templates: [
-            {
-              template: LENGTH_PASSWORD_TEMPLATE,
-              error: LENGTH_PASSWORD_ERROR,
-            },
-          ],
-        },
-        {
-          target: this.inputs['email'],
-          templates: [
-            {
-              template: EMAIL_TEMPLATE,
-              error: EMAIL_ERROR,
-            },
-          ],
+        const body = JSON.stringify({
+          login: this.inputs["name"].value,
+          password: this.inputs["password"].value,
+        });
 
-        },
-        {
-          target: this.inputs['name'],
-          templates: [
-            {
-              template: LOGIN_TEMPLATE,
-              error: LOGIN_ERROR,
-            },
-            {
-              template: LENGTH_LOGIN_TEMPLATE,
-              error: LENGTH_LOGIN_ERROR,
-            }
-            ,
-          ],
-        },
-      ]
+        console.log(body);
 
-      if (!this.validate_inputs(inputs_to_validate)) {
-        console.error('Some invalid inputs');
-        return
-      }
-
-      const body = JSON.stringify({
-        login: this.inputs['name'].value,
-        password: this.inputs['password'].value
-      });
-
-      console.log(body);
-
-      fetch(
-        API_V1_URL + 'signup',
-        {
+        fetch(API_V1_URL + "signup", {
           method: method,
           headers: headers,
-          body: body
-        }
-      ).then(response => {
-        console.log(response.headers);
-        if (response.status == 200) {
-          context.authenticated.status = true;
-          this.validationMsg.innerText = ""
-          inputs_to_validate.forEach((input) => {
-            input.target.value = ""
-          })
-          console.log("Signup succeed");
-        } else if (response.status == 401) {
-          this.validationMsg.innerText = USER_ALREADY_EXISTS_ERROR
-        } else {
-          this.validationMsg.innerText = SERVER_ERROR
-          console.error("Signup fatal error");
-        }
-      })
-
-    });
+          body: body,
+        }).then((response) => {
+          console.log(response.headers);
+          if (response.status == 200) {
+            context.authenticated.status = true;
+            this.validationMsg.innerText = "";
+            inputs_to_validate.forEach((input) => {
+              input.target.value = "";
+            });
+            console.log("Signup succeed");
+          } else if (response.status == 401) {
+            this.validationMsg.innerText = USER_ALREADY_EXISTS_ERROR;
+          } else {
+            this.validationMsg.innerText = SIGNUP_SERVER_ERROR;
+            console.error("Signup fatal error");
+          }
+        });
+      });
   }
 
   validate(input) {
     return input.templates.reduce((accumulator, template) => {
       if (input.target.value.match(template.template)) {
-        return accumulator && true
+        return accumulator && true;
       }
 
-      console.error("Target does not match template:", input.target.value, template.template)
-      this.validationMsg.innerText = template.error
+      console.error(
+        "Target does not match template:",
+        input.target.value,
+        template.template
+      );
+      this.validationMsg.innerText = template.error;
       return false;
     }, true);
   }
 
-  validate_inputs(inputs) {
+  validateInputs(inputs) {
     return inputs.reduce((acc, input) => {
       if (!this.validate(input)) {
-        input.target.style['border-width'] = '2px';
-        input.target.style['border-color'] = 'red';
-        return acc && false
+        input.target.style["border-width"] = "2px";
+        input.target.style["border-color"] = "red";
+        return acc && false;
       }
 
-      input.target.style['border-width'] = null;
-      input.target.style['border-color'] = null;
+      input.target.style["border-width"] = null;
+      input.target.style["border-color"] = null;
       return acc;
     }, true);
   }
