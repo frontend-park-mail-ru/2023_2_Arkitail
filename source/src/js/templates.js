319f87a18b01084a43d8983731cd39d394dee3dc
Arseniy Bannikov <bannikovtop@gmail.com>
2023-11-11 02:26:11 +0300

    Стили

--- source/src/js/templates.js
+++ source/src/js/templates.js
@@ -4,7 +4,7 @@ const TRIP_PAGE_TEMPLATE = `
     <h1 class='trip-name general-main-title'>{{name}}</h1>
     <div class='trip-manage'>
       <button class='trip-edit btn green-btn'>Редактировать</button>
-      <button class='trip-remove btn blue-btn'>Удалить</button>
+      <button class='trip-remove btn red-btn'>Удалить</button>
     </div>
   </div>
 </div>
@@ -14,54 +14,56 @@ const TRIP_PAGE_TEMPLATE = `
     <input class='trip-input-name input-field' name='name' type='text'/>
     <div class='trip-manage'>
       <button class='trip-save btn green-btn'>Сохранить</button>
-      <button class='trip-cancel btn gray-btn'>Отменить</button>
+      <button class='trip-cancel btn blue-btn'>Отменить</button>
     </div>
   </div>
 </div>
 
 <div class='trip-date'>{{begin}} - {{end}}</div>
 
-<div class='trip-description card'>
-  <h1>Описание</h1>
-  <p class='trip-toggle-first'>{{description}}</p>
-  <input class='trip-input-description trip-toggle-second' name='description' type='text'/>
-</div>
-
-<div class='trip-plan'>
-  <div class='trip-plan-header'>
-    <div class='trip-plan-name'>
-      <h1>План</h1>
+<div class='trip-content'>
+  <div class='trip-description card trip-card'>
+    <h1 class='general-title'>Описание</h1>
+    <p class='trip-toggle-first'>{{description}}</p>
+    <input class='trip-input-description trip-toggle-second' name='description' type='text'/>
+  </div>
+  
+  <div class='trip-plan card trip-card'>
+    <div class='trip-plan-header'>
+      <div class='trip-plan-name'>
+        <h1 class='general-title'>План</h1>
+      </div>
       <div class='trip-plan-new-point'>
-        <button>
+        <input class='trip-plan-new-point-id input-field' name='new-trip-id' placeholder=''>
+        <button class='btn blue-btn'>
           Добавить место
         </button>
-        <input class='trip-plan-new-point-id' name='new-trip-id' placeholder=''>
       </div>
     </div>
   </div>
-</div>
-
-<div class='trip-plan-days card'>
-  {{#eachInMap days}}
-  <div class='trip-plan-day'>
-    <div class='trip-plan-day-header'>
-      <p class='trip-plan-day-date'>{{this.key}}</p>
-      <button class='trip-plan-day-show'>Show/Hide</button>
-    </div>
-    <div class='trip-plan-points'>
-    {{#each this.value}}
-      <div class='trip-plan-point'>
-        <div class='trip-plan-point-header'><hr/>
-          <h1>Название метса</h1>
-          <p class='trip-plan-point-name'>{{this.name}}</p>
-          <h1>Описание</h1>
-          <p class='trip-plan-point-description'>{{this.description}}</p>
-        <hr/></div>
+  
+  <div class='trip-plan-days card'>
+    {{#eachInMap days}}
+    <div class='trip-plan-day'>
+      <div class='trip-plan-day-header'>
+        <p class='trip-plan-day-date'>{{this.key}}</p>
+        <button class='trip-plan-day-show'>Show/Hide</button>
       </div>
-    {{/each}}
+      <div class='trip-plan-points'>
+      {{#each this.value}}
+        <di trip-cardv class='trip-plan-point card trip-card'>
+          <div class='trip-plan-point-header'>
+            <h1>Название места</h1>
+            <p class='trip-plan-point-name'>{{this.name}}</p>
+            <h1>Описание</h1>
+            <p class='trip-plan-point-description'>{{this.description}}</p>
+          </div>
+        </div>
+      {{/each}}
+      </div>
     </div>
+    {{/eachInMap}}
   </div>
-  {{/eachInMap}}
 </div>`
 
 const TRIPS_PAGE_TEMPLATE = `
